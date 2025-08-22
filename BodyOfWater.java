9cfa5567226a2426c60c29133fe430c7b533e544
Jasmine Liu <jasmineliu1608@gmail.com>
2025-08-22 13:46:53 -0700

    Homework 1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,34 @@
+Class: BodyOfWater
+Attributes: 
+name, largestDiameter, avgDepth, isSaltWater
 
+Constructors:
+
+4 parameter constructor that allows all attributes to be set
+2 parameter constructor that sets name and avgDepth
+Methods:
+
+setName 
+setLargestDiameter
+printState
+
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private int avgDepth;
+    private boolean isSaltWater;
+
+    //Constructor
+    public BodyOfWater(String n, int largDiam, int aD, boolean isSalt){
+        name = n;
+        largestDiameter = largDiam;
+        avgDepth = aD;
+        isSaltWater = isSalt;
+    }
+    public ...
+
+    public void moing(){
+        name = 
+    }
+
+}
